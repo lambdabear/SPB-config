e52aef10c1c11bfd4cb0ea79b65a5bfef7e47fee
ZhangYi <zhangyi@goldensecure.com>
2019-04-11 00:11:04 +0800

    change config struct mask feild to prefix feild

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         Ok(mut config) => {
             println!("{:?}", config);
             config.ip_addr = "10.0.0.111".to_string();
-            config.mask = "255.255.255.0".to_string();
+            config.prefix = 24;
             config.gateway = "10.0.0.1".to_string();
             config.device_name = "农行人民街支行".to_string();
             save_config("./config.json", config).unwrap();
